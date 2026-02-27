d97d01bd8d796a3b87f5259ef0eba81ea73d1a79
HemantDahiyaCodes <HemantDahiyaCodes@gmail.com>
2026-02-27 11:00:20 +0000

    Change the index route to sign up to avoid confusion

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,17 @@
 import express from "express";
+import "dotenv/config";
 
 // Initalizing the app
 const app = express();
-const port = 3000;
 
 // File imports
-import "dotenv/config";
-import { index } from "./routes/indexRoute.js";
+import { index } from "./routes/signup.js";
 
 app.use(express.urlencoded({extended: true}));
 
 
 app.use("/", index);
 
-app.listen(Number(process.env.PORT) || port, () => {
-    console.log(`Server is running at ${process.env.PORT || port }`)
+app.listen(Number(process.env.PORT), () => {
+    console.log(`Server is running at ${process.env.PORT}`)
 })
--- backend/routes/indexRoute.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-const index = Router();
-
-index.get("/", (req, res) => {
-    res.send("This is working!");
-    console.log("index route works")
-})
-
-export {index};

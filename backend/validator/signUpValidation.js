d857e7c27f698e3a8662184fbd22d3fed2c8ac3d
HemantDahiyaCodes <HemantDahiyaCodes@gmail.com>
2026-06-02 17:40:25 +0000

    Create signup validation rules

---
+++ backend/validator/signUpValidation.js
@@ -0,0 +1,31 @@
+import { body } from "express-validator";
+import { prisma } from "../config/prismaClientConfig.js";
+
+export const validationRules = [
+  body("username")
+    .notEmpty()
+    .isLength({ min: 5, max: 25 })
+    .custom(async (value) => {
+      // checks whether a username already exists in the database and throws an error to avoid duplicate usernames
+      const user = await prisma.user.findUnique({ where: { username: value } });
+
+      if (user) {
+        throw new Error(`username ${user.username} already exists`);
+      }
+
+      return true;
+    }),
+  body("password")
+    .trim()
+    .notEmpty()
+    .isLength({ min: 8 })
+    .matches(/^(?=.*?[a-z])(?=.*?[0-9]).{8,}$/)
+    .withMessage(
+      "Password must be at least 8 characters long, contain letters and at least 1 number",
+    ),
+  body("email")
+    .trim()
+    .notEmpty()
+    .matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+    .withMessage("Invalid email entered"),
+];

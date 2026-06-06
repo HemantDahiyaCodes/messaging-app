fa320811accbed60148e6221761d6cc0e5fb66d6
HemantDahiyaCodes <HemantDahiyaCodes@gmail.com>
2026-06-06 16:20:25 +0000

    Create auth setup - Register login router - Initialize passport

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,20 @@
 import express from "express";
 import "dotenv/config.js";
+import {passport} from "./auth/passport.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares configurations
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(passport.initialize());
 
 // Routes
 import { signup } from "./routes/signup.js";
+import { loginRouter } from "./routes/login.js";
 
 app.use("/users", signup);
+app.use("/auth", loginRouter);
 
 app.listen(PORT, () => {
   console.log("The server is running!");

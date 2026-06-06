0137b2225a532a4d9e3892d9755b50f9f0eaf458
HemantDahiyaCodes <HemantDahiyaCodes@gmail.com>
2026-06-06 16:50:33 +0000

    Fix wrong status code in try block

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -10,18 +10,23 @@ loginRouter.post(
   (req, res) => {
     const { user } = req;
 
-    if (!user) {
-      return res
-        .status(400)
-        .json({ message: "Authentication failed, please log in again" });
-    }
+    try {
+      if (!user) {
+        return res
+          .status(401)
+          .json({ message: "Authentication failed, please log in again" });
+      }
 
-    const token = jwt.sign(
-      { id: user.id, username: user.username },
-      process.env.SECRET_KEY,
-      { expiresIn: "8h" },
-    );
-    return res.status(200).json({ success: true, token });
+      const token = jwt.sign(
+        { id: user.id, username: user.username }, // Creates a payload with payload.id, payload.username
+        process.env.SECRET_KEY,
+        { expiresIn: "8h" },
+      );
+
+      return res.status(200).json({ success: true, token });
+    } catch (error) {
+      return res.status(500).json("Server error");
+    }
   },
 );
 
